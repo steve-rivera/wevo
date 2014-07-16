1af89ecc8eb55702e9dc6fe838a02f09773ca4ed
hsg-dev <shobhit@highstartgroup.com>
2014-07-16 11:21:21 -0400

    Added external script for KISSMetrics

---
+++ scripts/scripts-external.js
@@ -0,0 +1,13 @@
+<script type="text/javascript">var _kmq = _kmq || [];
+var _kmk = _kmk || 'e6066118fb7bb389abb2169cb0c962cd6de79fb4';
+function _kms(u){
+  setTimeout(function(){
+    var d = document, f = d.getElementsByTagName('script')[0],
+    s = d.createElement('script');
+    s.type = 'text/javascript'; s.async = true; s.src = u;
+    f.parentNode.insertBefore(s, f);
+  }, 1);
+}
+_kms('//i.kissmetrics.com/i.js');
+_kms('//doug1izaerwt3.cloudfront.net/' + _kmk + '.1.js');
+</script>
