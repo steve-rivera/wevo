11ac5a3020f810e6db407fd6c4077ace21447ccb
hsg-dev <shobhit@highstartgroup.com>
2014-07-24 13:43:45 -0400

    Sign up button tracking

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -11,7 +11,7 @@ $( function() {
   thankYouSection = $('.js-thank-you-section');
 
   signUpButton.click( function() {
-    mixpanel.track("Learn More - top button");
+    mixpanel.track("Sign Up - top button");
     $('html, body').animate({
       scrollTop: signUpSection.offset().top
     }, 1200);
